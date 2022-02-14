1d05edc1714d2552a5d656da3a745a4f89a62d33
Faith Oodonghanro <toriboi.fo@gmail.com>
2022-02-14 03:46:19 +0100

    Implement functionality to get current user and by userId

--- src/interfaces/http/controller/userController.js
+++ src/interfaces/http/controller/userController.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 const {
-  StatusCodes: { OK, CREATED, BAD_REQUEST }
+  StatusCodes: { OK, CREATED, BAD_REQUEST, NOT_FOUND }
 } = require('http-status-codes')
 
 const container = require('../../../container')
@@ -16,6 +16,8 @@ class UserController {
     const router = Router()
     router.get('/users', authGuard, this.getAllUsers.bind(this))
     router.post('/user/create', authGuard, this.createANewUser.bind(this))
+    router.get('/user/me', authGuard, this.getCurrentUser.bind(this))
+    router.get('/user/:userId', authGuard, this.getUserById.bind(this))
     return router
   }
 
@@ -46,6 +48,26 @@ class UserController {
         }
         return next(error)
       })
+  }
+
+  getCurrentUser(req, res, next) {
+    return res.status(OK).send({ success: true, data: req.user.serialize() })
+  }
+
+  getUserById(req, res, next) {
+    const { userId } = req.params
+
+    this.userManagementService
+      .getUserById(userId)
+      .then((user) => {
+        if (!user) {
+          return res
+            .status(NOT_FOUND)
+            .json({ success: false, message: 'User Not Found' })
+        }
+        return res.status(OK).json({ success: true, data: user })
+      })
+      .catch((error) => next(error))
   }
 }
 
